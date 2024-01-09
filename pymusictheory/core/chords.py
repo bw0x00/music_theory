83f2bafe35e3044598bd99f337543b05946ce139
Bernd <bw@bw0x00.de>
2024-01-09 20:19:31 +0100

    extended predefined chords list

--- pymusictheory/core/chords.py
+++ pymusictheory/core/chords.py
@@ -9,11 +9,32 @@ from . import intervals
 # chord name to integeter notation mapping (semitone distance from root)
 chord_integer = {
     12: {
-        'powerchord'    : (0, 7),
-        'powerchord2'   : (0, 7, 12),
-        'major'         : (0, 4, 7),
-        'major7'        : (0, 4, 7, 11),
-        'minor'         : (0, 3, 7)
+        'powerchord'        : (0, 7),
+        'powerchord2'       : (0, 7, 12),
+        'major'             : (0, 4, 7),
+        "major6"            : (0, 4, 7, 9),
+        'major7'            : (0, 4, 7, 11),
+		"major9"            : (0, 4, 7, 11, 14),
+		"major11"           : (0, 4, 7, 11, 14, 17),
+		"major13"           : (0, 4, 7, 11, 14, 17, 21),
+		"sus2"              : (0, 2, 7),
+		"sus4"              : (0, 5, 7),
+		"dominant7"         : (0, 4, 7, 10),
+		"dominant9"         : (0, 4, 7, 10, 14),
+		"dominant11"        : (0, 4, 7, 10, 14, 17),
+		"dominant13"        : (0, 4, 7, 10, 14, 17, 21),
+        'minor'             : (0, 3, 7),
+		"minor6"            : (0, 3, 7, 9),
+		"minor7"            : (0, 3, 7, 10),
+		"minorM7"           : (0, 3, 7, 11),
+		"minor9"            : (0, 3, 7, 10, 14),
+		"minor11"           : (0, 3, 7, 10, 14, 17),
+		"diminished"        : (0, 3, 6),
+		"diminished7"       : (0, 3, 6, 9),
+		"half_diminished7"  : (0, 3, 6, 10),
+		"augmented"         : (0, 4, 8),
+		"augmented7"        : (0, 4, 8, 10),
+		"dominant7sus4"     : (0, 5, 7, 10)
     }
 }
 
